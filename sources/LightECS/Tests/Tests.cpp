c29bc71f208d118c2db567af1f58f6dda033527f
夷乐鑫 <1330958515@qq.com>
2024-10-18 17:30:28 +0800

    开发ECS模块中

---
+++ sources/LightECS/Tests/Tests.cpp
@@ -0,0 +1,52 @@
+﻿#include <vector>
+template <class...>
+struct Archetype
+{
+};
+
+template <class...>
+void RegisterArchetype()
+{
+}
+
+struct Position
+{
+    float x;
+    float y;
+};
+
+struct Velocity
+{
+    float x;
+    float y;
+};
+
+struct World
+{
+    template <class...>
+    static void AddEntities()
+    {
+    }
+
+    template <class...>
+    static void RemoveEntities()
+    {
+    }
+
+    template <class...>
+    static void ForeachEntities()
+    {
+    }
+
+    inline static std::vector<> positions = {};
+};
+
+template <Position, Velocity>
+struct Archetype
+{
+};
+
+void main()
+{
+    World::AddEntity<Position, Velocity>();
+}
