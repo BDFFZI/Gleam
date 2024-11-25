79786902c01fca5cb1604193547c925e2ee9a1e7
夷乐鑫 <1330958515@qq.com>
2024-11-25 17:30:53 +0800

    开发Reflection模块中

---
+++ sources/LightReflection/Tests/Tests.cpp
@@ -0,0 +1,43 @@
+﻿#include <typeindex>
+
+#include "LightMath/Runtime/Vector.hpp"
+class Transferrer
+{
+public:
+    virtual ~Transferrer() = default;
+    template <class TField>
+    void TransferField(const char* name, TField& value)
+    {
+        TransferField(name, value, typeid(TField));
+    }
+    void TransferField(const char* name, int& value)
+    
+    virtual void TransferValue(void& value, std::type_index type) = 0;
+    virtual void TransferBool(bool& value) = 0;
+    virtual void TransferChar(bool& value) = 0;
+    virtual void TransferInt(bool& value) = 0;
+    virtual void TransferFloat(bool& value) = 0;
+
+    virtual void PushPath() = 0;
+    virtual void PopPath() = 0;
+};
+class PrintTransmitter : public Transferrer
+{
+public:
+    void TransferField(const char* name, void& value, std::type_index type) override;
+};
+
+struct Data
+{
+    bool boolValue = true;
+    char charValue = 'A';
+    int intValue = 1;
+    float floatValue = 0.5f;
+    float3 float3Value = {1, 2, 3};
+    std::vector<bool> boolVectorValue = {false, true};
+};
+
+void main()
+{
+    Data data = {};
+}
