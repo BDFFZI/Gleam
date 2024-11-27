ead717f13dc8c03ba566d9930d46e65a88f41285
夷乐鑫 <1330958515@qq.com>
2024-11-27 17:45:01 +0800

    开发Reflection模块中

---
+++ sources/LightReflection/Runtime/Type.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+#include <memory>
+#include <string>
+#include <typeindex>
+#include <unordered_map>
+#include "uu"
+
+struct Type
+{
+    inline static std::unordered_map<std::type_index, std::unique_ptr<Type>> allTypes = {};
+
+    template <typename T>
+    static std::unique_ptr<Type>& Register()
+    {
+        allTypes.insert({typeid(T), std::make_unique<Type>()});
+        auto& type = allTypes.at(typeid(T));
+
+        type->typeInfo = typeid(T);
+        type->size = sizeof(T);
+        
+        return type;
+    }
+
+    uuid
+    std::type_info* typeInfo;
+    size_t size;
+};
