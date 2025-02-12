2dc10f0e397a4be58591cf0751aa84e4b6b259f6
BDFFZI <9080422+bdffzi@user.noreply.gitee.com>
2025-02-12 23:15:37 +0800

    -

--- sources/Libraries/GleamMath/Runtime/Geometry/Rectangle.cpp
+++ sources/Libraries/GleamMath/Runtime/Geometry/Rectangle.cpp
@@ -1,45 +1,45 @@
-﻿#include "Rect.h"
+﻿#include "Rectangle.h"
 
 #include "GleamMath/Runtime/LinearAlgebra/VectorMath.h"
 
 
 namespace Gleam
 {
-    Rect Rect::Zero = {};
+    Rectangle Rectangle::Zero = {};
 
-    Rect::Rect()
+    Rectangle::Rectangle()
         : x(0), y(0), width(0), height(0)
     {
     }
-    Rect::Rect(const float x, const float y, const float width, const float height)
+    Rectangle::Rectangle(const float x, const float y, const float width, const float height)
         : x(x), y(y), width(width), height(height)
     {
     }
-    Rect::Rect(const float2 position, const float2 size)
+    Rectangle::Rectangle(const float2 position, const float2 size)
         : x(position.x), y(position.y), width(size.x), height(size.y)
     {
     }
-    Rect::operator float4() const
+    Rectangle::operator float4() const
     {
         return {x, y, width, height};
     }
-    bool Rect::operator==(const Rect rect) const
+    bool Rectangle::operator==(const Rectangle rect) const
     {
         return all(static_cast<float4>(*this) == static_cast<float4>(rect));
     }
 
-    void Rect::SetSize(const float2 size)
+    void Rectangle::SetSize(const float2 size)
     {
         width = size.x;
         height = size.y;
     }
-    void Rect::SetPosition(const float2 position)
+    void Rectangle::SetPosition(const float2 position)
     {
         x = position.x;
         y = position.y;
     }
 
-    bool Rect::Contains(const float2 point) const
+    bool Rectangle::Contains(const float2 point) const
     {
         return point.x >= x && point.x - x <= width
             && point.y >= y && point.y - y <= height;
