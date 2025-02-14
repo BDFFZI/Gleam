6519e834dea27fb89f1614493879782bd377a7b1
BDFFZI <9080422+bdffzi@user.noreply.gitee.com>
2025-02-15 00:49:01 +0800

    -

---
+++ sources/Modules/GleamRendering/Editor/Gizmos.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "GleamUI/Runtime/UI.h"
+#include <ImGuizmo.h>
+
+#include "GleamEngine/Runtime/Component/Transform.h"
+#include "GleamMath/Runtime/LinearAlgebra/Matrix.h"
+
+namespace Gleam
+{
+    class Gizmos
+    {
+    public:
+        static float4x4& WorldToView();
+        static float4x4& ViewToClip();
+
+        static float3 DrawHandle(float3 position);
+        static void DrawHandle(ImGuizmo::OPERATION operation, float4x4 localToWorld, std::optional<std::reference_wrapper<LocalTransform>> transform);
+
+    private:
+        inline static float4x4 worldToView = float4x4::Identity();
+        inline static float4x4 viewToClip = float4x4::Identity();
+    };
+}
