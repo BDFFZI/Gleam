26e76a29008bc58f5edf4eb7f59bbc8d248a52b2
夷乐鑫 <1330958515@qq.com>
2025-04-03 09:01:22 +0800

    -

--- sources/Modules/GleamAssets/Editor/Configuration/ArtifactDB.h
+++ sources/Modules/GleamAssets/Editor/Configuration/ArtifactDB.h
@@ -9,7 +9,7 @@ namespace Gleam
         //在Unity中，该文件包含了导入依赖的信息，Artifact文件的meta信息以及Artifact文件信息。
         //不过目前，本引擎将这些信息作为.meta与Artifact文件存在了一起。所以本文件目前无效。
     };
-    Gleam_MakeType(ArtifactDB, "60B1C814-9CE3-4B50-883C-2B4ACC92F003")
+    Gleam_MakeTypeWithID(ArtifactDB, "60B1C814-9CE3-4B50-883C-2B4ACC92F003")
     {
     }
     Gleam_MakeConfiguration("Library/ArtifactDB.json", ArtifactDB)
--- sources/Modules/GleamAssets/Editor/Configuration/SourceAssetDB.h
+++ sources/Modules/GleamAssets/Editor/Configuration/SourceAssetDB.h
@@ -9,7 +9,7 @@ namespace Gleam
         //原文件的最后修改时间，文件内容的hash
         inline static std::unordered_map<std::filesystem::path, std::tuple<int64_t, uuids::uuid>> assetModificationInfos = {};
     };
-    Gleam_MakeType(SourceAssetDB, "011ED01E-45BE-4705-969A-22D3FD10252B")
+    Gleam_MakeTypeWithID(SourceAssetDB, "011ED01E-45BE-4705-969A-22D3FD10252B")
     {
         Gleam_MakeType_AddField(assetModificationInfos);
     }
